3b7f791b72f31acc6e9b9b7c18fa6691c4191c66
StarbornScribe <143401125+StarbornScribe@users.noreply.github.com>
2024-02-22 00:35:52 +0500

    Add files via upload

--- API Operations History/src/main/java/ru/netology/gutuev/Main.java
+++ API Operations History/src/main/java/ru/netology/gutuev/Main.java
@@ -1,8 +1,23 @@
 package ru.netology.gutuev;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("___Информация о транзакции___");
+        transaction_data();
+    }
 
-        System.out.println("___Начало проекта___");
+    public static void transaction_data(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи дату транзакции: ");
+        String dateScan = scanner.nextLine();
+        LocalDate date = LocalDate.parse(dateScan, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        System.out.println("Введите id транзакции: ");
+        int transaction_id = scanner.nextInt();
+        System.out.println("Введите сумму транзакции: ");
+        double transaction_sum = scanner.nextDouble();
+        System.out.println(date + " " + transaction_id + " " + transaction_sum);
     }
 }
