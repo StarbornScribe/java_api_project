9722af7d45db91a87f9262d5d0191fedb2b0447f
StarbornScribe <froliv982@yandex.ru>
2024-02-27 22:18:33 +0300

    Реализация ввода информации о транзациях через Scanner в цикле

--- API Operations History/src/main/java/ru/netology/gutuev/Main.java
+++ API Operations History/src/main/java/ru/netology/gutuev/Main.java
@@ -5,19 +5,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("___Информация о транзакции___");
         transaction_data();
     }
 
     public static void transaction_data(){
+        String transaction;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введи дату транзакции: ");
-        String dateScan = scanner.nextLine();
-        LocalDate date = LocalDate.parse(dateScan, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-        System.out.println("Введите id транзакции: ");
-        int transaction_id = scanner.nextInt();
-        System.out.println("Введите сумму транзакции: ");
-        double transaction_sum = scanner.nextDouble();
-        System.out.println(date + " " + transaction_id + " " + transaction_sum);
+
+        StringBuilder output = new StringBuilder();
+        String result;
+        while(true){
+            for (int i = 0; i<5; i++){
+                System.out.println("Введите id транзакции: ");
+                int transaction_id = scanner.nextInt();
+                scanner.nextLine(); // Этот сканер нужен для того, чтобы считать пустую строку после применения nextDouble() -- иначе выпадет ошибка
+
+                System.out.println("Введи дату транзакции: ");
+                String dateScan = scanner.nextLine();
+                LocalDate date = LocalDate.parse(dateScan, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+
+                System.out.println("Введите сумму транзакции: ");
+                double transaction_sum = scanner.nextDouble();
+
+                transaction = ("id:" + transaction_id + "| Дата: " + date + "| Сумма: " + transaction_sum);
+
+                output.append(transaction + '\n'); // Добавляет в StingBuilder output результаты текущего ввода тем самым накапливая там данные
+            }
+            result = output.toString(); // Превращает output в String после того как выполнен цикл for
+            break;
+        }
+        System.out.println("___Информация о транзакциях___" + '\n' + result);
     }
 }
